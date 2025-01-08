c5bce146687f342f4b6a8027cb6a5073a568d8c5
bolesygirl <bolesygirl@gmail.com>
2025-01-08 14:46:23 +1100

    Trying to get package working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,48 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ['../eslint-config/react.js'],
+  extends: ['eslint:recommended', 'prettier', 'turbo'],
+  plugins: ['only-warn', '@typescript-eslint'],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+    'dist/',
+    'tsup.config.ts',
+    'vitest.config.ts',
+  ],
+  overrides: [
+    {
+      files: ['*.js?(x)', '*.ts?(x)'],
+    },
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
-  overrides: [
-    {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/*.{spec,test}.{j,t}s?(x)'],
-    },
-  ],
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'none',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'no-redeclare': 'off',
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+  },
 };
